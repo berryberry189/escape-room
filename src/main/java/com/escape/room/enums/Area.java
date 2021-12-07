84e8383a8b2b69c9600d017ec1dd2937ea042ecb
berryberry189 <berryberry189@naver.com>
2021-12-07 20:57:02 +0900

    구역 타입 추가

--- src/main/java/com/escape/room/enums/Area.java
+++ src/main/java/com/escape/room/enums/Area.java
@@ -2,5 +2,19 @@ package com.escape.room.enums;
 
 public enum Area {
     강남 ,
-    홍대
+    홍대 ,
+    건대 ,
+    신림 ,
+    신촌 ,
+    잠실 ,
+    혜화 ,
+    합정 ,
+    인천 ,
+    경기 ,
+    부산 ,
+    전주 ,
+    대구 ,
+    광주 ,
+    양산
+
 }
